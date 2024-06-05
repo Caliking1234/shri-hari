b76a9bdaa2792ea974ae764e81da46e94577ec6c
Siddharth Gupta <siddharthguptaxyz@gmail.com>
2024-06-05 12:32:24 +0530

    ok...

--- src/app/components/aboutSection.jsx
+++ src/app/components/aboutSection.jsx
@@ -7,15 +7,15 @@ import docani from "/public/doc-ani.json";
 
 const AboutSection = () => {
   return (
-    <section className="w-full min-h-fit flex flex-col items-center justify-center bg-gray-100 py-10">
+    <section className="w-full min-h-screen flex flex-col items-center justify-center bg-gray-100 py-10">
       <div className="px-5 py-10 text-3xl md:text-5xl text-neutral-600 flex flex-col items-center justify-center gap-5 text-center">
         <h1>
           Why <span className=" text-[#233976] font-bold">ShriHari</span> Eye
           Care Center Is A Better Choice
         </h1>
       </div>
-      <div className=" w-full h-[70vh] flex flex-col md:flex-row relative py-5 overflow-hidden">
-        <div className=" w-full md:w-[40%] h-[50%] md:h-full overflow-hidden bg-red-500">
+      <div className=" w-full h-full flex flex-col md:flex-row relative py-5">
+        <div className=" w-full md:w-[40%] h-[50vh]">
           <Image
             objectFit="cover"
             src={img2}
@@ -39,7 +39,7 @@ const AboutSection = () => {
             <h1 className=" text-lg md:text-xl text-[#233976]">
               TEAM OF OVER 19 HIGHLY EXPERIENCED DOCTORS
             </h1>
-            <p className=" w-full md:text-sm text-neutral-500">
+            <p className=" w-full text-sm text-neutral-500">
               When you visit any of our hospitals, you have the collective
               experience of over 100+ doctors backing your treatments
             </p>
