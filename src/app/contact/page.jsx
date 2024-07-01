5d760c6439fcb339f6f983c1cdbd09b7a880942c
Siddharth Gupta <siddharthguptaxyz@gmail.com>
2024-07-01 11:55:13 +0530

    ..

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -70,14 +70,14 @@ const Page = () => {
     <>
       <div className="relative w-full pt-5 md:pt-0 h-[30vh] bg-[url('/images/img4.jpg')] bg-cover bg-center bg-no-repeat flex flex-row items-center justify-center gap-3  ">
         <div className=" w-full h-full absolute top-0 left-0 bg-gradient-to-r from-[#233976] via-[#233976b0] from-50%  to-transparent z-10"></div>
-        <div className=" z-[20] texty w-full h-full px-8 text-xl md:text-5xl capatalize tracking-tight leading-none text-white flex flex-col items-start justify-center gap-3 box-border">
+        <div className=" z-[20] texty w-1/2 h-full px-8 text-xl md:text-5xl capatalize tracking-tight leading-none text-white flex flex-col items-start justify-center gap-3 box-border">
           <div className=" w-full flex flex-row items-center justify-start">
             <h1 className=" text-[#F4BA0E] texty font-bold text-7xl">
               Contact Us
             </h1>
           </div>
         </div>
-        <div className=" texty w-full h-full">
+        <div className=" texty w-1/2 h-full">
           <Image
             src="/images/img4.jpg"
             fill
