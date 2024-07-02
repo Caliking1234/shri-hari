33ae3fcd27d009e995bc689c46a876e1005624dd
Siddharth Gupta <siddharthguptaxyz@gmail.com>
2024-07-02 09:32:33 +0530

    ..

--- src/app/components/cardCompnonent.jsx
+++ src/app/components/cardCompnonent.jsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 const CardCompnonent = ({ serviceName, ImageLink }) => {
   return (
-    <div className="relative group w-[160px] h-[250px] sm:w-[250px] sm:h-[200px] md:w-[300px] md:h-[300px] mx-auto">
+    <div className="relative group w-[160px] h-[200px] sm:w-[250px] sm:h-[200px] md:w-[300px] md:h-[300px] mx-auto">
       <div className=" w-full h-full absolute top-0 left-0 rounded-md flex flex-col z-10  bg-white px-1 py-3 shadow-xl hover:shadow-2xl">
         <Image
           src={ImageLink}
           width={300}
           height={300}
-          className="h-[150px] md:h-[250px] rounded-md"
+          className="h-[120px] md:h-[250px] rounded-md"
         />
         <h1 className=" text-center font-bold text-lg text-[#233976]">
           {serviceName}
